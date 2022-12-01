d6eae7af087d5c84fe1c7cf12652c69ea8411b81
Kaori318 <83631704+Kaori318@users.noreply.github.com>
2022-12-01 19:52:05 +0300

    lesson-1
    Создать компонент Message, отображающий переданный ему пропсом текст.
    
    Изменить компонент App так, чтобы тот рендерил Message и передавал ему пропсом текст
    (константу).

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,8 +1,14 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import MyHeader from './components/Message';
 
-export function App() {
+
+export function App(props) {
   return (
     <div className="App">
+      <header className="App-header">
+        <MyHeader name={"Ivan"}/>
+      </header>
     </div>
   );
 }
