580c0daa3e64176eb5bd419c085f7892b2e36967
Kaori318 <83631704+Kaori318@users.noreply.github.com>
2022-12-19 19:29:04 +0300

    les.3.2

--- les-2/src/App.js
+++ les-2/src/App.js
@@ -1,38 +1,112 @@
-import './App.css';
 import React, {useState, useEffect} from 'react'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import useStyles from './style'
+import InteractiveList from './components/Chat'
 import {Form} from './components/Form'
 import { Message } from './components/Message';
+import {AddUsers} from './components/AddUsers'
 
-function App() {
+
+export default function App() {
+  const classes = useStyles();
 
   const [messageList, setMessageList] = useState([])
   const [messageBody, setMessageBody] = useState({
-    text: '',
-    author: '',
-  })
+      text: '',
+      author: '',
+    })
+    const [users, setUser] = useState([])
 
-  const ROBOT_MESSAGE = 'Сообщение получено'
-
-  useEffect(() => {
-    if(messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot') {
-      setTimeout(()=>{
-        setMessageList(pervstate => [...pervstate, {text:ROBOT_MESSAGE, author: 'robot'}])
-      }, 1000)
+    const addUser = (newUser) => {
+      setUser([...users, newUser])
     }
-  },[messageList])
 
+
+    const ROBOT_MESSAGE = 'Сообщение получено'
+    
+    useEffect(() => {
+      if(messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot') {
+        setTimeout(()=>{
+          setMessageList(pervstate => [...pervstate, {text:ROBOT_MESSAGE, author: 'robot'}])
+        }, 1000)
+      }
+    },[messageList])
+  
   return (
-    <div className="App">
-      <div className="reverse">
+    <React.Fragment>
+      <CssBaseline />
+      <Container className={classes.container} maxWidth="md">
+        <div className='user'>
+          <AddUsers addUser={addUser}/>
+          <InteractiveList 
+          className={classes.chat} 
+          users={users}></InteractiveList>
+        </div>
+        <Typography
+        className={classes.message}>
+          <div className='messageList'>{
+          messageList.map((e,i)=><Message text={e.text} author={e.author} key={i}></Message>)
+      }
+    </div> 
+    <div className="reverse">
         <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
       </div>
-      <div className='messageList'>
-        {
-          messageList.map((e,i)=><Message text={e.text} author={e.author} key={i}></Message>)
-        }
-      </div> 
-    </div>
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
 
-export default App;
+
+
+ 
+
+  
+
+    // <div className="App">
+      
+    //   
+    // </div>
+// export default App;
+
+
+// import './App.css';
+// import React, {useState, useEffect} from 'react'
+
+
+// function App() {
+
+//   const [messageList, setMessageList] = useState([])
+//   const [messageBody, setMessageBody] = useState({
+//     text: '',
+//     author: '',
+//   })
+
+//   const ROBOT_MESSAGE = 'Сообщение получено'
+
+//   useEffect(() => {
+//     if(messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot') {
+//       setTimeout(()=>{
+//         setMessageList(pervstate => [...pervstate, {text:ROBOT_MESSAGE, author: 'robot'}])
+//       }, 1000)
+//     }
+//   },[messageList])
+
+//   return (
+// 
+//     // <div className="App">
+//     //   <div className="reverse">
+//     //     <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
+//     //   </div>
+//     //   <div className='messageList'>
+//     //     {
+//     //       messageList.map((e,i)=><Message text={e.text} author={e.author} key={i}></Message>)
+//     //     }
+//     //   </div> 
+//     // </div>
+//   );
+// }
+
+// export default App;
